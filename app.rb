75f2bed9ab523321737435c830f1acdd5142dea8
fenek644 <fenek644@ya.ru>
2019-01-23 02:48:38 +0300

    create @db in the space "befor"

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+# require 'sinatra/contrib'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'blog.sql'
+	@db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
